068588d85a92ce66c4f56f4680020a4ac78d5fc7
HikaruTakakura <takakura.hikaru@codeninth.com>
2019-09-26 20:41:36 +0900

    簡略化してCSStransitionきかせた

--- css-animation/expample/main.js
+++ css-animation/expample/main.js
@@ -1,14 +1,5 @@
-const display = document.getElementById("display")
-const squareButton = document.getElementById("square-button")
-const circleButton = document.getElementById("circle-button")
+const figure = document.getElementById("figure")
 
-
-squareButton.onclick = function (){
-    display.classList.remove('circle');
-    display.classList.add('square');
-};
-
-circleButton.onclick = function (){
-    display.classList.add('circle');
-    display.classList.remove('square');
+figure.onclick = function (){
+    figure.classList.toggle('circled');
 };
