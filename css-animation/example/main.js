53c88484b3cd15e2f399153bd9ae29431323d408
HikaruTakakura <takakura.hikaru@codeninth.com>
2019-09-28 16:09:05 +0900

    fix css-animation

--- css-animation/example/main.js
+++ css-animation/example/main.js
@@ -1,13 +1,5 @@
 const figure = document.getElementById("figure")
 
-let isSquare = true
-
 figure.onclick = function (){
-    if (isSquare) {
-        figure.classList.add('rounded')
-        isSquare = false
-    } else {
-        figure.classList.remove('rounded')
-        isSquare = true
-    }
+    figure.classList.toggle("rounded")
 }
