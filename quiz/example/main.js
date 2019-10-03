316be313588cb81a6827ca9b803ea6beabdba2a2
HikaruTakakura <takakura.hikaru@codeninth.com>
2019-10-03 16:17:00 +0900

    quiz強化

--- quiz/example/main.js
+++ quiz/example/main.js
@@ -1,25 +1,124 @@
-const answerContent = document.getElementById("answer-content")
-const fButton = document.getElementById("answer-1st")
-const gButton = document.getElementById("answer-2nd")
-const wButton = document.getElementById("answer-3rd")
-const hiddenContent = document.getElementById("hidden")
+// 登場人物たち
+const quizText = document.getElementById("quiz-text")
+const quizImage = document.getElementById("quiz-image")
+const choice1 = document.getElementById("choice-1")
+const choice2 = document.getElementById("choice-2")
+const choice3 = document.getElementById("choice-3")
+const feedback = document.getElementById("feedback")
+const nextQuizButton = document.getElementById("next-quiz")
 
+// 裏方さんたち
+let quizNumber = 0
+const quizzes = [
+    {
+        text: "この星の名前は何でしょう？",
+        image: "Ganymede.jpg",
+        choices: [
+            {
+                text: "ゴリアテ",
+                isCorrect: false,
+                feedback: "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。"
+            },
+            {
+                text: "ゼニガメ",
+                isCorrect: false,
+                feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。"
+            },
+            {
+                text: "ガニメデ",
+                isCorrect: true,
+                feedback: "正解！ガニメデは、木星の第三惑星だよ！"
+            }
+        ]
+    },
+    {
+        text: "いま、何問目？",
+        image: "Two.jpeg",
+        choices: [
+            {
+                text: "１",
+                isCorrect: false,
+                feedback: "おしい！ひとつ少ないよ。"
+            },
+            {
+                text: "２",
+                isCorrect: true,
+                feedback: "正解！１でも３でもないよ！"
+            },
+            {
+                text: "３",
+                isCorrect: false,
+                feedback: "おしい！ひとつ多いよ。"
+            }
+        ]
+    }
+]
 
-const answers = {
-    'f':'❌','g':'❌','w':'⭕️',
-};
+// 共通の処理①
+// quizNumber番目の問題を画面に表示する
+const reloadQuiz = function(quizNumber){
+    // quizzes[quizNumber] は使いまわすので、定数に入れておく
+    const quiz = quizzes[quizNumber]
 
-fButton.onclick = function(){
-    answerContent.textContent = answers.f
-    hiddenContent.classList.add('hidden')
-};
+    // 問題文を表示
+    quizText.textContent = quiz.text
 
-gButton.onclick = function(){
-    answerContent.textContent = answers.g
-    hiddenContent.classList.add('hidden')
-};
+    // 画像を表示
+    quizImage.src = "./images/" + quiz.image
 
-wButton.onclick = function(){
-    answerContent.textContent = answers.w
-    hiddenContent.classList.remove('hidden')
-};
+    // 選択肢（ボタン）の中身を表示
+    choice1.textContent = quiz.choices[0].text
+    choice2.textContent = quiz.choices[1].text
+    choice3.textContent = quiz.choices[2].text
+
+    // フィードバックを削除
+    feedback.textContent = ""
+
+    // 次の問題ボタンを隠す
+    nextQuizButton.classList.add("hidden")
+}
+
+// 共通の処理②
+// choiceNumber番目の選択肢を選択
+const choiced = function(choiceNumber) {
+    // quizzes[quizNumber].choices[choiceNumber] は使いまわすので、定数に入れておく
+    const choice = quizzes[quizNumber].choices[choiceNumber]
+
+    // フィードバックを表示
+    feedback.textContent = choice.feedback
+
+    // 正解 かつ 次の問題があれば、次の問題ボタンを表示
+    if(choice.isCorrect){
+        if(quizNumber < quizzes.length-1){
+            // 次の問題ボタンを表示
+            nextQuizButton.classList.remove("hidden")
+        } else {
+            // 結果表示
+
+        }
+    }
+}
+
+// 次の問題ボタンを押したら
+nextQuizButton.onclick = function(){
+    // 問題番号を１増やす
+    quizNumber += 1
+    // quizNumber番目の問題を読み込む
+    reloadQuiz(quizNumber)
+}
+
+choice1.onclick = function() {
+    // 0番目の選択肢を選択
+    choiced(0)
+}
+choice2.onclick = function() {
+    // 1番目の選択肢を選択
+    choiced(1)
+}
+choice3.onclick = function() {
+    // 2番目の選択肢を選択
+    choiced(2)
+}
+
+// 0番目のクイズを表示
+reloadQuiz(0)
