334cd5701d8d06e32f062cb013fca39f29a23241
HikaruTakakura <takakura.hikaru@codeninth.com>
2019-12-18 18:59:38 +0000

    インデントを調整

--- example/1/□と○/main.js
+++ example/1/□と○/main.js
@@ -1,5 +1,5 @@
 const figure = document.getElementById("figure")
 
 figure.onclick = function (){
-    figure.classList.toggle("rounded")
+  figure.classList.toggle("rounded")
 }
--- example/1/ストップウォッチ/main.js
+++ example/1/ストップウォッチ/main.js
@@ -4,19 +4,19 @@ const button = document.getElementById("button")
 let count = 0
 
 const countUp = function(){
-    count += 1
-    display.textContent = count/100
+  count += 1
+  display.textContent = count/100
 }
 
 let id = null
 
 button.onclick = function(){
-    if (id === null) { // start
-        id = setInterval(countUp, 10)
-        button.textContent = "stop"
-    } else { // stop
-        clearInterval(id)
-        id = null
-        button.textContent = "start"
-    }
+  if (id === null) { // start
+    id = setInterval(countUp, 10)
+    button.textContent = "stop"
+  } else { // stop
+    clearInterval(id)
+    id = null
+    button.textContent = "start"
+  }
 }
