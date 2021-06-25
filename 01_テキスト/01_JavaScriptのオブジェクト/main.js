f6517c6f388f48a2b647cb09d4f1514a9970c313
HikaruTakakura <takakurahikaru@gmail.com>
2021-06-25 16:04:53 +0900

    add JavaScriptのオブジェクト

---
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -0,0 +1,16 @@
+const hikakin = {
+  name: "ヒカキン",
+  age: 24,
+  hobbies: ["ゲーム", "動画編集", "猫と遊ぶこと"],
+  isHappy: true,
+  address: {
+    country: "日本",
+    city: "東京"
+  },
+  sayHello: function() {
+    console.log("ブンブンハロー YouTube")
+  }
+}
+
+// 練習問題
+// hikakin オブジェクトに入っている sayHello メソッドを実行してください。
